53673efcb804f29dc28a9be20daa088d59237350
sreyas <sreyas1045@gmail.com>
2024-10-07 22:05:10 +0530

    added some code

--- src/components/TaskerCard/TaskCardDashboard.tsx
+++ src/components/TaskerCard/TaskCardDashboard.tsx
@@ -6,25 +6,28 @@ type TaskType = {
   status: string;
 };
 
-import { Flex, Text} from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 const TaskCardDashboard = ({ task }: { task: TaskType }) => {
   return (
     <Flex
-    padding={4}
+      padding={4}
       bg={"brand.secondary"}
       flex={2}
       alignItems={"start"}
       justifyContent={"center"}
       color={"brand.textPrimary"}
-      fontFamily={'Montserrat'}
+      fontFamily={"Montserrat"}
       borderRadius={3}
-      direction={'column'}
+      direction={"column"}
       gap={3}
     >
-      <Flex >{task?.task}</Flex>
-      <Flex  gap={2} alignItems={'baseline'} justifyContent={'center'}  >
-        status : <Text borderRadius={3} bg={'brand.error'} py={1} px={2}>{task?.status}</Text>
+      <Flex>{task?.task}</Flex>
+      <Flex gap={2} alignItems={"baseline"} justifyContent={"center"}>
+        status :{" "}
+        <Text borderRadius={3} bg={"brand.error"} py={1} px={2}>
+          {task?.status}
+        </Text>
       </Flex>
     </Flex>
   );
